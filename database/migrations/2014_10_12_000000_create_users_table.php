6c294c62795dfabd7a8e87261393d44ebdfdbe79
Amirhossein <Amirmemool12@gmail.com>
2023-08-28 14:30:08 +0330

    :bug: fix: fix a little mistake

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,7 +23,7 @@ return new class extends Migration {
             $table->string('address')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->enum('role', ['admin', 'agent', 'user'])->default('user');
-            $table->enum('status', ['active', 'inactive', 'user'])->default('active');
+            $table->enum('status', ['active', 'inactive'])->default('active');
             $table->rememberToken();
             $table->timestamps();
         });
