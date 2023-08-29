243c8e7e7e1d4e502d16b5d5e67b7c5415e63ec5
Amirhossein <Amirmemool12@gmail.com>
2023-08-29 18:41:18 +0330

    :lipstick: style: show the admin pannel with the index method

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,1 +1,21 @@
-<h1>Admin Dashboard</h1>
+<!DOCTYPE html>
+
+<html lang="en">
+
+    @include('admin.header-dashboard')
+
+<body>
+<div class="main-wrapper">
+
+    @include('admin.sidebar-dashboard')
+
+    @include('admin.navbar-dashboard')
+
+    @yield('content')
+
+</div>
+
+    @include('admin.footer-dashboard')
+
+</body>
+</html>
