b8e35b96fd7730456bc056177f959bc541bd3967
Amirhossein <Amirmemool12@gmail.com>
2023-08-29 23:46:53 +0330

    :card_file_box: DB: create Profile migratoin

---
+++ database/migrations/2023_08_29_192020_create_profiles_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('profile_image')->nullable();
+            $table->string('header_image')->nullable();
+            $table->text('about')->nullable();
+            $table->string('address')->nullable();
+            $table->string('github')->nullable();
+            $table->string('instagram')->nullable();
+            $table->string('tweeter')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
