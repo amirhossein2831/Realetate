ce91b1ab761eddcc75236fb22ccd1aa5ccabd9ac
Amirhossein <Amirmemool12@gmail.com>
2023-08-29 19:20:58 +0330

    :hammer: chore: define a route to logout the admin

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AgentController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +17,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Admin group route
+Route::prefix('admin')->middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+    Route::post('/logout',[AdminController::class,'destroy'])->name('admin.logout');
+});
+
+//Agent grout route
+Route::prefix('agent')->middleware(['auth', 'role:agent'])->group(function () {
+    Route::get('/dashboard', [AgentController::class, 'dashboard'])->name('agent.dashboard');
+});
+
+//User grout route
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -23,19 +43,5 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::prefix('admin')->middleware(['auth', 'role:admin'])->group(function () {
-    Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
-});
-
-Route::prefix('agent')->middleware(['auth', 'role:agent'])->group(function () {
-    Route::get('/dashboard', [AgentController::class, 'dashboard'])->name('agent.dashboard');
-});
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 require __DIR__ . '/auth.php';
